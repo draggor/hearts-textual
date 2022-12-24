7fd40b187422dd377bacae1cc782a540d6d4322d
Draggor <draggor@gmail.com>
2022-12-24 13:53:28 -0600

    workin on tests

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,3 +1,22 @@
+import pytest
+
+from hearts_textual.commands import run_command, Message
+
+
+@pytest.fixture
+def echo():
+    return '{"command": "echo", "args": {"message": "honk"}}'
+
+
+@pytest.fixture
+def websocket():
+    return {}
+
+
 class TestCommands:
-    def test_default(self):
-        assert True
+    def test_run_echo(self, echo, websocket, capsys):
+        result = run_command(echo, websocket)
+
+        captured = capsys.readouterr()
+
+        assert captured.out == "Broadcast: honk\n"
