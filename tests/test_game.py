9479f64506b723583351b2640085bbcfc65e691b
Draggor <draggor@gmail.com>
2022-12-24 02:28:52 -0600

    Begin game testing

---
+++ tests/test_game.py
@@ -0,0 +1,19 @@
+from hearts_textual.data import Game
+
+import pytest
+
+class TestGame:
+
+    @pytest.fixture(autouse=True)
+    def new_game(self):
+        self.game = Game()
+        self.game.reset()
+
+    def test_reset(self):
+        game = self.game
+
+        assert game.round == 0
+        assert len(game.deck) == 52
+        assert game.lead_player is None
+        assert len(game.players) == 4
+        assert len(game.played_cards) == 0
