17569cb328353f72ffcd963517be62c5b7ee175c
Draggor <draggor@gmail.com>
2024-07-22 23:31:29 -0500

    continue the experiment, make it actually run

--- tui/game_screen.py
+++ tui/game_screen.py
@@ -10,112 +10,6 @@ from hearts_textual import data
 from tui.base_screen import BaseScreen
 
 
-demo_game = data.Game.from_dict(
-    {
-        "round": 1,
-        "turn": 1,
-        "started": True,
-        "ended": False,
-        "hearts_broken": False,
-        "deck": [],
-        "lead_player": 0,
-        "players": [
-            {
-                "name": "Homer",
-                "connected": True,
-                "hand": [
-                    {"suit": "C", "value": "2"},
-                    {"suit": "C", "value": "6"},
-                    {"suit": "C", "value": "T"},
-                    {"suit": "C", "value": "A"},
-                    {"suit": "D", "value": "5"},
-                    {"suit": "D", "value": "9"},
-                    {"suit": "D", "value": "K"},
-                    {"suit": "S", "value": "4"},
-                    {"suit": "S", "value": "8"},
-                    {"suit": "S", "value": "Q"},
-                    {"suit": "H", "value": "3"},
-                    {"suit": "H", "value": "7"},
-                    {"suit": "H", "value": "J"},
-                ],
-                "play": None,
-                "pile": [],
-                "scores": [],
-            },
-            {
-                "name": "Goose",
-                "connected": True,
-                "hand": [
-                    {"suit": "C", "value": "3"},
-                    {"suit": "C", "value": "7"},
-                    {"suit": "C", "value": "J"},
-                    {"suit": "D", "value": "2"},
-                    {"suit": "D", "value": "6"},
-                    {"suit": "D", "value": "T"},
-                    {"suit": "D", "value": "A"},
-                    {"suit": "S", "value": "5"},
-                    {"suit": "S", "value": "9"},
-                    {"suit": "S", "value": "K"},
-                    {"suit": "H", "value": "4"},
-                    {"suit": "H", "value": "8"},
-                    {"suit": "H", "value": "Q"},
-                ],
-                "play": None,
-                "pile": [],
-                "scores": [],
-            },
-            {
-                "name": "Penguin",
-                "connected": True,
-                "hand": [
-                    {"suit": "C", "value": "4"},
-                    {"suit": "C", "value": "8"},
-                    {"suit": "C", "value": "Q"},
-                    {"suit": "D", "value": "3"},
-                    {"suit": "D", "value": "7"},
-                    {"suit": "D", "value": "J"},
-                    {"suit": "S", "value": "2"},
-                    {"suit": "S", "value": "6"},
-                    {"suit": "S", "value": "T"},
-                    {"suit": "S", "value": "A"},
-                    {"suit": "H", "value": "5"},
-                    {"suit": "H", "value": "9"},
-                    {"suit": "H", "value": "K"},
-                ],
-                "play": None,
-                "pile": [],
-                "scores": [],
-            },
-            {
-                "name": "Menace",
-                "connected": True,
-                "hand": [
-                    {"suit": "C", "value": "5"},
-                    {"suit": "C", "value": "9"},
-                    {"suit": "C", "value": "K"},
-                    {"suit": "D", "value": "4"},
-                    {"suit": "D", "value": "8"},
-                    {"suit": "D", "value": "Q"},
-                    {"suit": "S", "value": "3"},
-                    {"suit": "S", "value": "7"},
-                    {"suit": "S", "value": "J"},
-                    {"suit": "H", "value": "2"},
-                    {"suit": "H", "value": "6"},
-                    {"suit": "H", "value": "T"},
-                    {"suit": "H", "value": "A"},
-                ],
-                "play": None,
-                "pile": [],
-                "scores": [],
-            },
-        ],
-        "turn_order": [0, 1, 2, 3],
-        "played_cards": [],
-        "summary": {},
-    }
-)
-
-
 class Header(Placeholder):
     pass
 
@@ -144,23 +38,25 @@ class PlayArea(Container):
     p2card = reactive(PlayCard(None, id="p2card"))
     p3card = reactive(PlayCard(None, id="p3card"))
     p4card = reactive(PlayCard(None, id="p4card"))
-    game = reactive(None, recompose=True)
+    game: data.Game = reactive(None, recompose=True)
 
-    def __init__(self, game) -> None:
+    def __init__(self, game: data.Game) -> None:
         super().__init__()
         self.game = game
 
     def watch_game(self, game) -> None:
-        self.game = game
-        for pcard, card in zip([p1card, p2card, p3card, p4card], game.played_cards):
-            pcard.card = card
+        if game is not None:
+            for pcard, card in zip(
+                [self.p1card, self.p2card, self.p3card, self.p4card], game.played_cards
+            ):
+                pcard.card = card
 
     def compose(self) -> ComposeResult:
         # Docks for player names
-        yield Static(demo_game.players[1].name, id="P2")
-        yield Static(demo_game.players[3].name, id="P4")
-        yield Static(demo_game.players[0].name, id="P1")
-        yield Static(demo_game.players[2].name, id="P3")
+        yield Static(self.game.players[1].name, id="P2")
+        yield Static(self.game.players[3].name, id="P4")
+        yield Static(self.game.players[0].name, id="P1")
+        yield Static(self.game.players[2].name, id="P3")
 
         # Grid
         # Top Left
@@ -229,15 +125,19 @@ class Hand(HorizontalScroll):
 
     cards = reactive([])
     selected = reactive(-1)
+    game = reactive(None, recompose=True)
 
     class PlayCardMessage(Message):
         def __init__(self, card: data.Card) -> None:
             self.card = card
             super().__init__()
 
-    def __init__(self, cards: list[data.Card], *, id: str = ""):
+    def __init__(self, game: data.Game, *, id: str = ""):
         super().__init__()
 
+        self.game = game
+
+        cards = self.game.players[0].hand
         self.cards = [Card(card, in_hand=True, id=repr(card)) for card in cards]
         self.cards.sort(key=lambda card: card.card)
 
@@ -251,8 +151,8 @@ class Hand(HorizontalScroll):
     @on(Card.Pressed, ".hand_selected")
     def remove_card(self, event: Button.Pressed) -> None:
         card = event.button.card
-        player = demo_game.players[0]
-        game_or_error = demo_game.play_card(card, player)
+        player = self.game.players[0]
+        game_or_error = self.game.play_card(card, player)
 
         if isinstance(game_or_error, str):
             self.query_one("#Footer").update(game_or_error)
@@ -275,10 +175,10 @@ class Hand(HorizontalScroll):
 
 class GameScreen(Screen):
 
-    hand = demo_game.players[0].hand
-    self.game = reactive(None, recompose=True)
+    # hand = demo_game.players[0].hand
+    game: data.Game = reactive(None, recompose=True)
 
-    def __init__(self, game):
+    def __init__(self, game: data.Game):
         super().__init__()
         self.game = game
 
@@ -286,9 +186,9 @@ class GameScreen(Screen):
         with BaseScreen():
             # Without nested container, Hand docks to bottom over footer in BaseScreen
             with Container():
-                yield Hand(self.game, id="Hand")
-                yield PlayArea(self.game, id="PlayArea")
+                yield Hand(self.game)
+                yield PlayArea(self.game)
 
     @on(Hand.PlayCardMessage)
     def handle_play_card(self, message: Hand.PlayCardMessage) -> None:
-        self.query_one("#PlayArea").play_card(message.card)
+        self.query_one(PlayArea).play_card(message.card)
